8c07a3241ec9f7aa9f8cf62f3babf1814517d9c1
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-11-13 18:32:49 +0100

    Create clyster turret

--- src/ferrum/Ferrum.kt
+++ src/ferrum/Ferrum.kt
@@ -17,6 +17,7 @@ class Ferrum : Mod() {
     lateinit var oreIron: OreBlock
     lateinit var iron: Item
     lateinit var canna: ItemTurret
+    lateinit var clyster: ItemTurret
 
     override fun loadContent() {
         iron = Item("iron", Color.valueOf("7f786e")).apply {
@@ -33,29 +34,78 @@ class Ferrum : Mod() {
         canna = ItemTurret("canna").apply {
             requirements(Category.turret, ItemStack.with(iron, 35))
             ammo(
-                Items.lead, BasicBulletType(2.5f, 31f).apply {
+                Items.lead,
+                BasicBulletType(2.5f, 31f).apply {
                     knockback = 1.6f
-                    lifetime = 60f
+                    lifetime = 64f
                     height = 14f
                     width = height
                     pierce = true
                     pierceCap = 2
                     reloadMultiplier = 1.2f
-                }, iron, BasicBulletType(3.5f, 45f).apply {
+                },
+                iron,
+                BasicBulletType(3.5f, 45f).apply {
                     knockback = 1.6f
                     lifetime = 50f
                     height = 14f
                     width = height
                     pierce = true
                     pierceCap = 3
-            },)
+                },
+            )
             targetAir = false
             reload = 90f
             recoil = 2f
-            range = 142f
+            range = 146f
             inaccuracy = 2.5f
             shootCone = 8f
             health = 360
+            shootSound = Sounds.cannon
+        }
+
+        clyster = ItemTurret("clyster").apply {
+            requirements(Category.turret, ItemStack.with(iron, 25, Items.graphite, 25))
+            ammo(
+                Items.metaglass,
+                BasicBulletType(3f, 16f).apply {
+                    lifetime = 60f
+                    height = 14f
+                    width = height
+
+                    fragBullet = BasicBulletType(6f, 5f).apply {
+                        lifetime = 12f
+
+                        pierce = true
+                        pierceCap = 2
+                    }
+                    fragBullets = 7
+
+                    reloadMultiplier = 1.25f
+                },
+                iron,
+                BasicBulletType(2.5f, 22f).apply {
+                    knockback = 2f
+                    lifetime = 60f
+                    height = 14f
+                    width = height
+
+                    fragBullet = BasicBulletType(4f, 10f).apply {
+                        lifetime = 12f
+
+                        pierce = true
+                        pierceCap = 2
+                    }
+                    fragBullets = 5
+                }
+            )
+            targetAir = false
+            reload = 100f
+            recoil = 2f
+            range = 142f
+            inaccuracy = 4f
+            shootCone = 12f
+            health = 320
             shootSound = Sounds.cannon
         }
 
