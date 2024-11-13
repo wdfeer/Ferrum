1853e2ef9ed23949e60bcef3f94805bfcb18619e
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-11-13 13:49:05 +0100

    Add iron as requirement for some vanilla blocks

--- src/ferrum/Ferrum.kt
+++ src/ferrum/Ferrum.kt
@@ -1,8 +1,10 @@
 package ferrum
 
 import arc.graphics.Color
+import mindustry.content.Blocks
 import mindustry.mod.Mod
 import mindustry.type.Item
+import mindustry.type.ItemStack
 import mindustry.world.blocks.environment.OreBlock
 
 class Ferrum : Mod(){
@@ -19,6 +21,31 @@ class Ferrum : Mod(){
             oreDefault = true
             oreThreshold = 0.85f
             oreScale = 25f
+        }
+
+        Blocks.steamGenerator.apply {
+            requirements = requirements.plus(ItemStack(iron, 15))
+        }
+        Blocks.thoriumReactor.apply {
+            requirements = requirements.plus(ItemStack(iron, 100))
+        }
+
+        Blocks.blastDrill.apply {
+            requirements = requirements.plus(ItemStack(iron, 40))
+        }
+
+        Blocks.multiPress.apply {
+            requirements = requirements.plus(ItemStack(iron, 25))
+        }
+        Blocks.plastaniumCompressor.apply {
+            requirements = requirements.plus(ItemStack(iron, 30))
+        }
+
+        Blocks.meltdown.apply {
+            requirements = requirements.plus(ItemStack(iron, 70))
+        }
+        Blocks.spectre.apply {
+            requirements = requirements.plus(ItemStack(iron, 90))
         }
     }
 }
