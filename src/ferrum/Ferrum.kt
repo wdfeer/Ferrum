17810c061cf8eef17979b2023d5c7036acc95395
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-11-13 13:36:21 +0100

    Create iron item

--- src/ferrum/Ferrum.kt
+++ src/ferrum/Ferrum.kt
@@ -1,9 +1,16 @@
 package ferrum
 
-import mindustry.mod.*
+import arc.graphics.Color
+import mindustry.mod.Mod
+import mindustry.type.Item
+import mindustry.world.blocks.environment.OreBlock
 
 class Ferrum : Mod(){
     override fun loadContent(){
         TODO("Create content")
+        iron = Item("iron", Color.valueOf("7f786e")).apply {
+            hardness = 3
+            cost = 1f
+        }
     }
 }
