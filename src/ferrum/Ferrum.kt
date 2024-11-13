67740d04366d349851ad7aafc2f4349110d98889
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-11-13 13:40:23 +0100

    Create iron ore

--- src/ferrum/Ferrum.kt
+++ src/ferrum/Ferrum.kt
@@ -6,11 +6,19 @@ import mindustry.type.Item
 import mindustry.world.blocks.environment.OreBlock
 
 class Ferrum : Mod(){
+    lateinit var oreIron: OreBlock
+    lateinit var iron: Item
+
     override fun loadContent(){
-        TODO("Create content")
         iron = Item("iron", Color.valueOf("7f786e")).apply {
             hardness = 3
             cost = 1f
+        }
+
+        oreIron = OreBlock(iron).apply {
+            oreDefault = true
+            oreThreshold = 0.85f
+            oreScale = 25f
         }
     }
 }
