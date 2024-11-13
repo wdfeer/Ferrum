aa2188e445f059b75b69b43e9a8d3c542980a06c
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-11-13 18:35:34 +0100

    Rebalance turrets

--- src/ferrum/Ferrum.kt
+++ src/ferrum/Ferrum.kt
@@ -35,23 +35,25 @@ class Ferrum : Mod() {
             requirements(Category.turret, ItemStack.with(iron, 35))
             ammo(
                 Items.lead,
-                BasicBulletType(2.5f, 31f).apply {
+                BasicBulletType(2.75f, 31f).apply {
                     knockback = 1.6f
                     lifetime = 64f
                     height = 14f
                     width = height
                     pierce = true
                     pierceCap = 2
-                    reloadMultiplier = 1.2f
+                    reloadMultiplier = 1.3f
+                    collidesAir = false
                 },
                 iron,
-                BasicBulletType(3.5f, 45f).apply {
+                BasicBulletType(3.25f, 42f).apply {
                     knockback = 1.6f
                     lifetime = 50f
                     height = 14f
                     width = height
                     pierce = true
                     pierceCap = 3
+                    collidesAir = false
                 },
             )
             targetAir = false
@@ -68,8 +70,8 @@ class Ferrum : Mod() {
             requirements(Category.turret, ItemStack.with(iron, 25, Items.graphite, 25))
             ammo(
                 Items.metaglass,
-                BasicBulletType(3f, 16f).apply {
-                    lifetime = 60f
+                BasicBulletType(4f, 14f).apply {
+                    lifetime = 50f
                     height = 14f
                     width = height
 
@@ -81,16 +83,16 @@ class Ferrum : Mod() {
                     }
                     fragBullets = 7
 
-                    reloadMultiplier = 1.25f
+                    reloadMultiplier = 1.3f
                 },
                 iron,
-                BasicBulletType(2.5f, 22f).apply {
-                    knockback = 2f
+                BasicBulletType(3f, 22f).apply {
+                    knockback = 1.6f
                     lifetime = 60f
                     height = 14f
                     width = height
 
-                    fragBullet = BasicBulletType(4f, 10f).apply {
+                    fragBullet = BasicBulletType(4f, 8f).apply {
                         lifetime = 12f
 
                         pierce = true
@@ -99,7 +101,6 @@ class Ferrum : Mod() {
                     fragBullets = 5
                 }
             )
-            targetAir = false
             reload = 100f
             recoil = 2f
             range = 142f
