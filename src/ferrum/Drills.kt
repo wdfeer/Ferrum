cbed6d67fb408ff090c9b7feee391b4ca93408c0
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-11-21 11:28:24 +0100

    Put drills in Drills.kt

---
+++ src/ferrum/Drills.kt
@@ -0,0 +1,100 @@
+package ferrum
+
+import arc.struct.Seq
+import mindustry.content.*
+import mindustry.content.TechTree.TechNode
+import mindustry.game.Objectives.SectorComplete
+import mindustry.type.Category
+import mindustry.type.ItemStack
+import mindustry.world.Tile
+import mindustry.world.blocks.production.Drill
+import mindustry.world.meta.Stat
+import mindustry.world.meta.StatValues
+
+fun Ferrum.addDrills() {
+    pyriteExtractor = object : Drill("pyrite-extractor") {
+        init {
+            researchCost = ItemStack.with(Items.copper, 1000, Items.lead, 500, Items.graphite, 200)
+            alwaysUnlocked = false
+            techNode = TechNode(Blocks.mechanicalDrill.techNode, this, researchCost).also {
+                it.objectives = Seq.with(SectorComplete(SectorPresets.frozenForest))
+            }
+        }
+
+        override fun canMine(tile: Tile?): Boolean {
+            return tile?.drop() == Items.coal
+        }
+
+        override fun countOre(tile: Tile?) {
+            super.countOre(tile)
+            returnItem = pyrite
+        }
+
+        override fun setStats() {
+            super.setStats()
+
+            stats.remove(Stat.drillTier)
+            stats.add(
+                Stat.drillTier, StatValues.drillables(
+                drillTime, hardnessDrillMultiplier,
+                (size * size).toFloat(), drillMultipliers
+            ) { it.itemDrop == Items.coal })
+        }
+    }.apply {
+        requirements(
+            Category.production,
+            ItemStack.with(Items.lead, 100, Items.graphite, 30)
+        )
+        drillTime = 280f
+        size = 3
+        hasPower = true
+        tier = 4
+        updateEffect = Fx.pulverizeMedium
+        drillEffect = Fx.mineBig
+
+        consumePower(0.5f)
+        consumeLiquid(Liquids.cryofluid, 0.1f).boost()
+    }
+
+    ironExtractor = object : Drill("iron-extractor") {
+        init {
+            researchCost = ItemStack.with(Items.copper, 2000, iron, 500, Items.silicon, 500)
+            alwaysUnlocked = false
+            techNode = TechNode(pyriteExtractor.techNode, this, researchCost)
+        }
+
+        override fun canMine(tile: Tile?): Boolean {
+            return tile?.drop() == Items.titanium
+        }
+
+        override fun countOre(tile: Tile?) {
+            super.countOre(tile)
+            returnItem = iron
+        }
+
+        override fun setStats() {
+            super.setStats()
+
+            stats.remove(Stat.drillTier)
+            stats.add(
+                Stat.drillTier, StatValues.drillables(
+                drillTime, hardnessDrillMultiplier,
+                (size * size).toFloat(), drillMultipliers
+            ) { it.itemDrop == Items.titanium })
+        }
+    }.apply {
+        requirements(
+            Category.production,
+            ItemStack.with(Items.copper, 80, Items.silicon, 30, iron, 20)
+        )
+        drillTime = 280f
+        size = 3
+        hasPower = true
+        tier = 4
+        updateEffect = Fx.pulverizeMedium
+        drillEffect = Fx.mineBig
+
+        consumePower(2f)
+        consumeLiquid(Liquids.cryofluid, 0.1f).boost()
+    }
+}
--- src/ferrum/Ferrum.kt
+++ src/ferrum/Ferrum.kt
@@ -2,17 +2,17 @@ package ferrum
 
 import arc.graphics.Color
 import arc.struct.Seq
-import mindustry.content.*
+import mindustry.content.Blocks
+import mindustry.content.Fx
+import mindustry.content.Items
 import mindustry.content.TechTree.TechNode
 import mindustry.game.Objectives.Produce
-import mindustry.game.Objectives.SectorComplete
 import mindustry.gen.Sounds
 import mindustry.mod.Mod
 import mindustry.type.Category
 import mindustry.type.Item
 import mindustry.type.ItemStack
 import mindustry.world.Block
-import mindustry.world.Tile
 import mindustry.world.blocks.defense.turrets.ItemTurret
 import mindustry.world.blocks.environment.OreBlock
 import mindustry.world.blocks.power.SolarGenerator
@@ -22,8 +22,6 @@ import mindustry.world.blocks.units.Reconstructor
 import mindustry.world.draw.DrawDefault
 import mindustry.world.draw.DrawGlowRegion
 import mindustry.world.draw.DrawMulti
-import mindustry.world.meta.Stat
-import mindustry.world.meta.StatValues
 
 class Ferrum : Mod() {
     lateinit var oreIron: OreBlock
@@ -84,92 +82,6 @@ class Ferrum : Mod() {
         addTurrets()
 
         modifyVanillaContent()
-    }
-
-    private fun addDrills() {
-        pyriteExtractor = object : Drill("pyrite-extractor") {
-            init {
-                researchCost = ItemStack.with(Items.copper, 1000, Items.lead, 500, Items.graphite, 200)
-                alwaysUnlocked = false
-                techNode = TechNode(Blocks.mechanicalDrill.techNode, this, researchCost).also {
-                    it.objectives = Seq.with(SectorComplete(SectorPresets.frozenForest))
-                }
-            }
-
-            override fun canMine(tile: Tile?): Boolean {
-                return tile?.drop() == Items.coal
-            }
-
-            override fun countOre(tile: Tile?) {
-                super.countOre(tile)
-                returnItem = pyrite
-            }
-
-            override fun setStats() {
-                super.setStats()
-
-                stats.remove(Stat.drillTier)
-                stats.add(Stat.drillTier, StatValues.drillables(
-                    drillTime, hardnessDrillMultiplier,
-                    (size * size).toFloat(), drillMultipliers
-                ) { it.itemDrop == Items.coal })
-            }
-        }.apply {
-            requirements(
-                Category.production,
-                ItemStack.with(Items.lead, 100, Items.graphite, 30)
-            )
-            drillTime = 280f
-            size = 3
-            hasPower = true
-            tier = 4
-            updateEffect = Fx.pulverizeMedium
-            drillEffect = Fx.mineBig
-
-            consumePower(0.5f)
-            consumeLiquid(Liquids.cryofluid, 0.1f).boost()
-        }
-
-        ironExtractor = object : Drill("iron-extractor") {
-            init {
-                researchCost = ItemStack.with(Items.copper, 2000, iron, 500, Items.silicon, 500)
-                alwaysUnlocked = false
-                techNode = TechNode(pyriteExtractor.techNode, this, researchCost)
-            }
-
-            override fun canMine(tile: Tile?): Boolean {
-                return tile?.drop() == Items.titanium
-            }
-
-            override fun countOre(tile: Tile?) {
-                super.countOre(tile)
-                returnItem = iron
-            }
-
-            override fun setStats() {
-                super.setStats()
-
-                stats.remove(Stat.drillTier)
-                stats.add(Stat.drillTier, StatValues.drillables(
-                    drillTime, hardnessDrillMultiplier,
-                    (size * size).toFloat(), drillMultipliers
-                ) { it.itemDrop == Items.titanium })
-            }
-        }.apply {
-            requirements(
-                Category.production,
-                ItemStack.with(Items.copper, 80, Items.silicon, 30, iron, 20)
-            )
-            drillTime = 280f
-            size = 3
-            hasPower = true
-            tier = 4
-            updateEffect = Fx.pulverizeMedium
-            drillEffect = Fx.mineBig
-
-            consumePower(2f)
-            consumeLiquid(Liquids.cryofluid, 0.1f).boost()
-        }
     }
 
     private fun modifyVanillaContent() {
