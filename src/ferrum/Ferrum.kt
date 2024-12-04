35829c03719dd6aaf89d174ed1882b022c685424
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-12-04 20:58:31 +0100

    Put modifyVanillaContent in Vanilla.kt

--- src/ferrum/Ferrum.kt
+++ src/ferrum/Ferrum.kt
@@ -12,17 +12,14 @@ import mindustry.mod.Mod
 import mindustry.type.Category
 import mindustry.type.Item
 import mindustry.type.ItemStack
-import mindustry.world.Block
 import mindustry.world.blocks.defense.turrets.ItemTurret
 import mindustry.world.blocks.environment.OreBlock
-import mindustry.world.blocks.power.PowerGenerator
 import mindustry.world.blocks.production.Drill
 import mindustry.world.blocks.production.GenericCrafter
 import mindustry.world.draw.DrawDefault
 import mindustry.world.draw.DrawFlame
 import mindustry.world.draw.DrawGlowRegion
 import mindustry.world.draw.DrawMulti
-import kotlin.math.round
 
 class Ferrum : Mod() {
     lateinit var oreIron: OreBlock
@@ -147,116 +144,5 @@ class Ferrum : Mod() {
         addTurrets()
 
         modifyVanillaContent()
-    }
-
-    private fun modifyVanillaContent() {
-        // Pyrite
-        run {
-            (Blocks.solarPanel as PowerGenerator).powerProduction *= 1.2f
-            (Blocks.largeSolarPanel as PowerGenerator).powerProduction *= 1.2f
-
-            fun addPyriteRequirement(block: Block, amount: Int) {
-                block.requirements = block.requirements.plus(ItemStack(pyrite, amount))
-            }
-
-            addPyriteRequirement(Blocks.solarPanel, 1)
-            addPyriteRequirement(Blocks.batteryLarge, 30)
-            addPyriteRequirement(Blocks.largeSolarPanel, 15)
-            addPyriteRequirement(Blocks.phaseWeaver, 80)
-            addPyriteRequirement(Blocks.foreshadow, 400)
-            addPyriteRequirement(Blocks.multiplicativeReconstructor, 300)
-        }
-
-        // Pyratite
-        run {
-            (Blocks.pyratiteMixer as GenericCrafter).consumeItems(ItemStack(pyrite, 1))
-            (Blocks.impactReactor as PowerGenerator).powerProduction *= 1.1f
-            (Blocks.differentialGenerator as PowerGenerator).powerProduction *= 1.25f
-
-
-            fun ItemTurret.buffAmmo(vararg items: Item) {
-                ammoTypes.forEach { ammo ->
-                    if (items.contains(ammo.key)) {
-                        ammo.value.apply {
-                            damage = round(damage * 1.1f)
-                            splashDamage = round(splashDamage * 1.1f)
-                        }
-                    }
-                }
-            }
-
-            // Buff pyratite and blast compound as ammo
-            listOf(
-                Blocks.scorch,
-                Blocks.hail,
-                Blocks.salvo,
-                Blocks.ripple,
-                Blocks.swarmer,
-                Blocks.cyclone,
-                Blocks.spectre
-            ).filterIsInstance<ItemTurret>().forEach { it.buffAmmo(Items.pyratite, Items.blastCompound) }
-        }
-
-        // Iron
-        run {
-            fun addIronRequirement(block: Block, amount: Int) {
-                block.requirements = block.requirements.plus(ItemStack(iron, amount))
-            }
-
-            addIronRequirement(Blocks.steamGenerator, 15)
-            addIronRequirement(Blocks.laserDrill, 15)
-            addIronRequirement(Blocks.multiPress, 35)
-            addIronRequirement(Blocks.exponentialReconstructor, 600)
-        }
-
-        // Steel
-        run {
-            (Blocks.largeSolarPanel as PowerGenerator).powerProduction *= 1.2f
-
-            fun addSteelRequirement(block: Block, amount: Int) {
-                block.requirements = block.requirements.plus(ItemStack(steel, amount))
-            }
-
-            addSteelRequirement(Blocks.largeSolarPanel, 40)
-            addSteelRequirement(Blocks.thoriumReactor, 100)
-            addSteelRequirement(Blocks.impactReactor, 500)
-            addSteelRequirement(Blocks.blastDrill, 25)
-            addSteelRequirement(Blocks.plastaniumCompressor, 30)
-            addSteelRequirement(Blocks.meltdown, 150)
-            addSteelRequirement(Blocks.spectre, 100)
-            addSteelRequirement(Blocks.tetrativeReconstructor, 1600)
-            addSteelRequirement(Blocks.coreNucleus, 2000)
-        }
-
-        // Tech Tree
-        run {
-            fun TechNode.addReq(amount: Int, item: Item) {
-                requirements = requirements.plus(ItemStack(item, amount))
-                if (finishedRequirements.size < requirements.size)
-                    finishedRequirements = finishedRequirements.plus(ItemStack(item, 0))
-            }
-
-            Blocks.solarPanel.techNode.addReq(50, pyrite)
-            Blocks.pyratiteMixer.techNode.addReq(200, pyrite)
-            Blocks.batteryLarge.techNode.addReq(200, pyrite)
-            Blocks.largeSolarPanel.techNode.addReq(800, pyrite)
-            Blocks.multiplicativeReconstructor.techNode.addReq(1000, pyrite)
-            Blocks.foreshadow.techNode.addReq(5000, pyrite)
-
-
-            Blocks.steamGenerator.techNode.addReq(150, iron)
-            Blocks.laserDrill.techNode.apply {
-                parent = pyriteExtractor.techNode
-                addReq(50, iron)
-            }
-            Blocks.thoriumReactor.techNode.addReq(500, iron)
-            Blocks.exponentialReconstructor.techNode.addReq(5000, iron)
-
-
-            Blocks.blastDrill.techNode.addReq(500, steel)
-            Blocks.spectre.techNode.addReq(3000, steel)
-            Blocks.meltdown.techNode.addReq(3000, steel)
-            Blocks.tetrativeReconstructor.techNode.addReq(10000, steel)
-        }
     }
 }
