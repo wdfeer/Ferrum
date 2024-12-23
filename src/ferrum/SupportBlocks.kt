ef797c21642a902944b2787c8b7f3756dd00f47a
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-12-23 06:31:38 +0100

    Make iron dome splash

--- src/ferrum/SupportBlocks.kt
+++ src/ferrum/SupportBlocks.kt
@@ -1,17 +1,24 @@
 package ferrum
 
 import arc.func.Prov
+import arc.graphics.Color
+import arc.graphics.g2d.Draw
+import arc.graphics.g2d.Lines
+import arc.math.Angles
 import arc.math.Mathf
 import arc.math.geom.Vec2
-import arc.struct.Seq
 import arc.util.Tmp
 import ferrum.util.noneBuilt
 import mindustry.content.Blocks
-import mindustry.content.Fx
 import mindustry.content.Items
 import mindustry.content.Liquids
+import mindustry.entities.Effect
+import mindustry.entities.Effect.EffectContainer
 import mindustry.gen.Bullet
 import mindustry.gen.Groups
+import mindustry.gen.Sounds
+import mindustry.graphics.Drawf
+import mindustry.graphics.Pal
 import mindustry.type.Category
 import mindustry.type.ItemStack
 import mindustry.world.blocks.defense.OverdriveProjector
@@ -40,31 +47,44 @@ fun Ferrum.loadSupportBlocks() {
     }.apply {
         buildType = Prov {
             object : PointDefenseTurret.PointDefenseBuild() {
+                val hitSound = Sounds.plasmaboom
+                val splashRadius = 80f
                 override fun updateTile() {
-                    reloadCounter += delta()
-
-                    val targets = Groups.bullet.intersect(x - range, y - range, range * 2, range * 2)
+                    target = Groups.bullet.intersect(x - range, y - range, range * 2, range * 2)
                         .select { it.team !== team && it.type().hittable && it.within(this, range) }
+                        .max { b: Bullet -> b.damage } ?: return
 
-                    if (!targets.isEmpty && reloadCounter >= reload) attack(targets)
+                    val dest = angleTo(target)
+                    rotation = Angles.moveToward(rotation, dest, rotateSpeed * edelta())
+                    reloadCounter += edelta()
+                    if (Angles.within(rotation, dest, shootCone) && reloadCounter >= reload) attack(target)
                 }
 
-                fun attack(targets: Seq<Bullet>) {
-                    targets.forEach {
-                        it.remove()
+                fun attack(target: Bullet) {
+                    target.remove()
+                    splash(target.x, target.y)
 
-                        beamEffect.at(x + Tmp.v1.x, y + Tmp.v1.y, rotation, color, Vec2().set(it))
-                        shootEffect.at(x + Tmp.v1.x, y + Tmp.v1.y, rotation, color)
-                        if (it.damage > 25f) hitEffect.at(it.x, it.y, color)
-                        shootSound.at(x + Tmp.v1.x, y + Tmp.v1.y, Mathf.random(0.9f, 1.1f))
-                    }
+                    Tmp.v1.trns(rotation, shootLength)
+
+                    beamEffect.at(x + Tmp.v1.x, y + Tmp.v1.y, rotation, color, Vec2().set(target))
+                    shootEffect.at(x + Tmp.v1.x, y + Tmp.v1.y, rotation, color)
+                    hitEffect.at(target.x, target.y, color)
+                    hitSound.at(target)
+                    shootSound.at(x + Tmp.v1.x, y + Tmp.v1.y, Mathf.random(0.9f, 1.1f))
                     reloadCounter = 0f
+                }
+
+                fun splash(x: Float, y: Float) {
+                    Groups.bullet.intersect(
+                        x - splashRadius, y - splashRadius, splashRadius * 2, splashRadius * 2
+                    ).select { it.team !== team && it.type().hittable && it.within(target, splashRadius) }
+                        .forEach { it.remove() }
                 }
             }
         }
         requirements(
             Category.effect, ItemStack.with(
-                Items.titanium, 8000, steel, 8000, Items.silicon, 6000, Items.phaseFabric, 2500, mischmetal, 1600
+                steel, 8000, Items.silicon, 6000, Items.thorium, 6000, Items.phaseFabric, 2500, mischmetal, 1600
             )
         )
         health = 12000
@@ -72,8 +92,23 @@ fun Ferrum.loadSupportBlocks() {
         hasPower = true
         consumePower(100f)
         consumeLiquid(Liquids.cryofluid, 0.5f)
-        hitEffect = Fx.smokeCloud
+        hitEffect = Effect(40f, 100f) { e: EffectContainer ->
+            Draw.color(Color.white)
+            Lines.stroke(e.fout() * 2f)
+            val circleRad = 4f + e.finpow() * 65f
+            Lines.circle(e.x, e.y, circleRad)
+
+            for (i in 0..3) {
+                Drawf.tri(e.x, e.y, 6f, 100f * e.fout(), (i * 90).toFloat())
+            }
+
+            for (i in 0..3) {
+                Drawf.tri(e.x, e.y, 3f, 35f * e.fout(), (i * 90).toFloat())
+            }
+            Drawf.light(e.x, e.y, circleRad * 1.6f, Pal.techBlue, e.fout())
+        }
         size = 5
+        rotateSpeed *= 3f
         reload = 10f
     }
 }
